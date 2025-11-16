e0a71e193eee1fa2c023b2983cd1467a689cef4f
xrptor <gafurov.kirill@gmail.com>
2025-11-16 23:23:50 +0300

    Исправление ошибок

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -21,7 +21,7 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
             else:
                 base = ord("a")
 
-            shift = ord(key_char.upper()) - ord("A")
+            shift = ord(key_char.upper()) - ord("a")
 
             new_char = (ord(char) - base + shift) % 26 + base
             ciphertext += chr(new_char)
@@ -55,7 +55,7 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
             else:
                 base = ord("a")
 
-            shift = ord(key_char.upper()) - ord("A")
+            shift = ord(key_char.upper()) - ord("a")
 
             original_char = (ord(char) - base - shift + 26) % 26 + base
             plaintext += chr(original_char)
