98ed4af6af6c876b64e5152245914da8ca876617
xrptor <gafurov.kirill@gmail.com>
2025-12-16 03:09:05 +0300

    fixes

--- homework03/maze.py
+++ homework03/maze.py
@@ -363,5 +363,6 @@ if __name__ == "__main__":
     GRID = bin_tree_maze(15, 15)
     print(pd.DataFrame(GRID))
     MAZE, PATH = solve_maze(GRID)
-    MAZE = add_path_to_grid(MAZE, PATH)
+    if PATH:
+        MAZE = add_path_to_grid(MAZE, PATH)
     print(pd.DataFrame(MAZE))
